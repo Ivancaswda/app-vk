a5fb5d506b729b4049aea17f15f1e024a500c034
ScienceProject.io <162120681+Ivancaswda@users.noreply.github.com>
2025-04-12 13:06:33 +0300

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance= axios.create({
-    baseURL: import.meta.env.NODE_ENV === 'development' ? 'http://localhost:4023/api' : '/api',
+    baseURL: import.meta.env.NODE_ENV === 'development' ? 'http://localhost:2100/api' : '/api',
     withCredentials: true,
 })
 export default axiosInstance
